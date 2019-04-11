8359c5190d72c74021ca7e67d7df69bbfe2d7e46
Arkko002 <arkko002@gmail.com>
2019-04-11 12:54:43 +0200

    CategoriesWithQuantityReport and tests

---
+++ Lombard.Logic/CategoriesWithQuantityReport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lombard.Logic
+{
+    public class CategoriesWithQuantityReport
+    {
+        public Dictionary<string, int> GroupByNames(Items itemsToGroup)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lombard.Tests/CategoriesWithQuantityReportTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Lombard.Logic;
+
+namespace Lombard.Tests
+{
+    class CategoriesWithQuantityReportTests
+    {
+        [Test]
+        public void GroupByNames_PopulatedList_ReturnsGroupedDictionary()
+        {
+            var catReport = new CategoriesWithQuantityReport();
+
+            Items items = new Items();
+           var item = new Item()
+            {
+                Name = "TestName",
+                Quantity = 2
+            };
+
+            var item2 = new Item()
+            {
+                Name = "TestName",
+                Quantity = 2
+            };
+
+            items.ListOfItems.Add(item);
+            items.ListOfItems.Add(item2);
+
+            var result = catReport.GroupByNames(items);
+
+            Assert.IsTrue(result.ContainsKey("TestName"));
+            Assert.IsTrue(result["TestName"] == 4);
+        }
+
+        [Test]
+        public void GroupByNames_EmptyList_ReturnsEmptyDictionary()
+        {
+            var catReport = new CategoriesWithQuantityReport();
+
+        }
+
+
+        [Test]
+        public void GroupByQuantity_PopulatedList_ReturnsGroupedDictionary()
+        {
+            var catReport = new CategoriesWithQuantityReport();
+
+        }
+
+        [Test]
+        public void GroupByQuantity_EmptyList_ReturnsEmptyDictionary()
+        {
+
+        }
+    }
+}

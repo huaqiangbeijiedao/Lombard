e3c5ac0c7633d1aba08b1af4ba8a704cae1432c2
Marcin Trocha <cinekmar21@gmail.com>
2019-04-11 15:50:57 +0200

    Fix

---
+++ Lombard.API/Models/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lombard.API.Models
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+        public List<Product> Products { get; set; }
+        public DateTime TransactionDate { get; set; } 
+    }
+}
---
+++ Lombard.Logic/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Lombard.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lombard.Logic.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}

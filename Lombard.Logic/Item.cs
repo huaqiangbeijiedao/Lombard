2ddfe9f27b0e77453e394d3ef6b818756d849406
Marcin Trocha <cinekmar21@gmail.com>
2019-04-11 12:47:59 +0200

    Added WareHuseQuantity method

--- Lombard.Logic/Item.cs
+++ Lombard.Logic/Item.cs
@@ -4,11 +4,21 @@ using System.Text;
 
 namespace Lombard.Logic
 {
-    class Item
+    public class Item
     {
         public int ProductId { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
         public int Quantity { get; set; }
+
+        static public int WareHouseQuantity(IEnumerable<Item> items)
+        {
+            int quantinty = 0;
+            foreach(var i in items)
+            {
+                quantinty += i.Quantity; 
+            }
+            return quantinty;
+        }
     }
 }
--- Lombard.Tests/UnitTest1.cs
+++ Lombard.Tests/UnitTest1.cs
@@ -1,20 +1,25 @@
+using Lombard.Logic;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
+       
         [Test]
-        public void Test1()
+  
+        public void WareHouseQuantity_ItemsList_CorrectQuantity()
         {
-            Assert.Pass();
+            var listOfItems = new List<Item>
+            {
+                new Item {ProductId = 1,Price = 5.00, Quantity = 1},
+                new Item {ProductId = 2,Price = 25.00, Quantity = 2},
+                new Item {ProductId = 3,Price = 6.00, Quantity = 5}
+            };
+            var itemsCount = Item.WareHouseQuantity(listOfItems);
+            Assert.AreEqual(8,itemsCount);
         }
-
 
 
     }

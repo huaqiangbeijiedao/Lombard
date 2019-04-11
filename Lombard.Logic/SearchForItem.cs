92f87d4000697c91fa37ee37a626920845efc963
Arkko002 <arkko002@gmail.com>
2019-04-11 13:47:29 +0200

    SearchForItems and tests

---
+++ Lombard.Logic/SearchForItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lombard.Logic
+{
+    class SearchForItem
+    {
+        public Items SearchForItems(Items items)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lombard.Tests/SearchForItemTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lombard.Tests
+{
+    class SearchForItemTests
+    {
+        [Test]
+        public void SearchForItem_ExistingItem_FindsItem()
+        {
+
+        }
+
+        [Test]
+        public void SearchForItem_EmptyItem_DoesntFindItem()
+        {
+
+        }
+    }
+}

69a78f95ba0848f962ce866ef4f2b810c3e0273f
Krystian Kijak <kry.kjjk@op.pl>
2019-04-15 11:11:23 +0200

    CS8019

--- Lombard.API/Controllers/TransactionController.cs
+++ Lombard.API/Controllers/TransactionController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Lombard.API.Models;
 using Lombard.API.Repository;
 using Lombard.API.Wrapper;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lombard.API.Controllers
